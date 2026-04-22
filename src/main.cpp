6071fd98c133fa87c1c443dbbcb3c4b9c1fd3948
pongkhunwuttijarat-droid <pongkhunwuttijarat@gmail.com>
2026-04-22 22:34:25 +0700

    Refactor main.cpp to initialize BMI160 sensor via SPI and remove WiFi connection code

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,24 @@
 #include <Arduino.h>
-#include <WiFi.h>
+#include <BMI160Gen.h>
+#include <SPI.h>
 
-const char* ssid = "POCOF5";
-const char* password = "seal1234";
+// กำหนดขา CS (Chip Select)
+const int select_pin = 5; 
 
 void setup() {
-  Serial.begin(115200);
+    Serial.begin(115200);
+    Serial.println("Initializing BMI160...");
+    // สำหรับ ESP32 แนะนำให้เริ่มต้น SPI เองก่อนเพื่อความแม่นยำของขา
+    // VSPI Default: SCK=18, MISO=19, MOSI=23, SS=5
+    SPI.begin(); 
 
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi");
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print("connecting...");
-  }
-
-  Serial.println("\nConnected!");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
+    // เริ่มต้นเซนเซอร์ในโหมด SPI
+    if (!BMI160.begin(BMI160GenClass::SPI_MODE, select_pin)) {
+        Serial.println("BMI160 connection failed!");
+        while (1);
+    }
+    Serial.println("BMI160 connected via SPI!");
 }
-
 void loop() {
-  // nothing here
+
 }
